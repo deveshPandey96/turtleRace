661bce38d98acc8a4036eac932c6a530721f631a
Devesh Pandey <deveshpandey787@gmail.com>
2024-11-01 14:14:29 +0530

    completed initial lineups of all turtles

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
-screen = Screen()
-screen.setup(width = 500, height = 400)
 
+screen = Screen()
+screen.setup(width = 1000, height = 800)
+user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
+print(user_bet)
+colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+for turtle_index in range(0,7):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(-480, 350-(100*turtle_index))
 
 
 screen.exitonclick()
